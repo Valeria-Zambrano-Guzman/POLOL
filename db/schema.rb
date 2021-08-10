4347534def38306b565789c2f7e240bef0c6c3f9
Fernando E. Silva Jacquier <fesilvajacquier@protonmail.com>
2021-08-10 17:54:55 -0300

    feat(Users): Add Customer & Professional
    This commit also adds the necessary devise routes.
    Missing specific views. Necessary to edit a professsional's profile.

--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_06_220812) do
+ActiveRecord::Schema.define(version: 2021_08_10_195334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "professionals", force: :cascade do |t|
-    t.string "name"
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "first_name"
     t.string "last_name"
-    t.integer "age"
-    t.string "email"
-    t.date "date_of_birth"
-    t.integer "phone"
-    t.string "city"
+    t.date "birthdate"
+    t.string "phone_number"
     t.string "address"
-    t.string "occupation"
-    t.boolean "certification"
-    t.text "description"
+    t.float "longitude"
+    t.float "latitude"
+    t.string "type", default: "Customer"
+    t.boolean "certification", default: false, null: false
+    t.boolean "background_check", default: false, null: false
+    t.text "biography"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
