cbc5c7fc7904047ff82faea7374f53f5c6ed72ac
Francesca Ahumada <javiera.francesca@gmail.com>
2021-08-11 18:23:48 -0400

    Changing components and pages front-end

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def uikit
+  end
+
+  def type
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   devise_for :customers
   devise_for :professionals
 
+  get 'uikit', to: 'pages#uikit'
+  get 'type', to: 'pages#type'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
