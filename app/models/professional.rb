c3825765ecb2c4c2ded6419ad868747bae0053df
hipolito huaylla <huaylla7@hotmail.com>
2021-08-13 18:14:02 -0400

    mapa creado

--- app/models/professional.rb
+++ app/models/professional.rb
@@ -1,6 +1,4 @@
 class Professional < User
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
 
   has_many :reviews, class_name: "Review", foreign_key: "professional_id", dependent: :destroy
   has_many :appointments, class_name: "Appointment", foreign_key: "professional_id", dependent: :destroy
