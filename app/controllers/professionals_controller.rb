3b17b9d808890183a4558a5290b0e5976c7eaaf2
hipolito huaylla <huaylla7@hotmail.com>
2021-08-18 17:19:45 -0400

    mapa v3

--- app/controllers/professionals_controller.rb
+++ app/controllers/professionals_controller.rb
@@ -4,7 +4,9 @@ class ProfessionalsController < ApplicationController
     if params[:sub_service_id].present?
       @professionals = @professionals.joins(:specialities).where(specialities: { sub_service_id: params[:sub_service_id] })
     end
-
+    if params[:query].present?
+      @professionals = @professionals.near(params[:query], 100)
+    end
     # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
     @markers = @professionals.geocoded.map do |user|
       {
